c7d37eaa9e91842c6661cb31a15f074149aa9d38
Sanju A <your-email@example.com>
2026-09-01 19:27:46 +0530

    Fix home page route

--- server.js
+++ server.js
@@ -58,7 +58,9 @@ app.get("/contacts", async (req, res) => {
         });
     }
 });
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
